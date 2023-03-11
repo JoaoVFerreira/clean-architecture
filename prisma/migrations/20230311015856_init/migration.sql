400e0d20e699f22d998667638288b373557b4488
João Ferreira <joaovtf39@gmail.com>
2023-03-10 23:39:16 -0300

    refactor: add a missing column in init migration

--- prisma/migrations/20230311015856_init/migration.sql
+++ prisma/migrations/20230311015856_init/migration.sql
@@ -13,6 +13,7 @@ CREATE TABLE "Coupon" (
     "id" SERIAL NOT NULL,
     "code" TEXT NOT NULL,
     "percentage" INTEGER NOT NULL,
+    "expire_date" TIMESTAMP NOT NULL
 
     CONSTRAINT "Coupon_pkey" PRIMARY KEY ("id")
 );
@@ -25,4 +26,4 @@ INSERT INTO "Product" ("id_product", "description", "price") VALUES (1, 'A', 100
 INSERT INTO "Product" ("id_product", "description", "price") VALUES (2, 'B', 5000);
 INSERT INTO "Product" ("id_product", "description", "price") VALUES (3, 'C', 30);
 
-INSERT INTO "Coupon" ("code", "percentage") VALUES ('VALE20', 20);
+INSERT INTO "Coupon" ("code", "percentage", "expire_date") VALUES ('VALE20', 20, '2022-12-01T10:00:00');
