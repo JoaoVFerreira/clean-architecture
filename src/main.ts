54b078451ebce869ca519c7633864ca7919621e0
João Ferreira <joaovtf39@gmail.com>
2023-03-10 23:27:08 -0300

    fix: remove .env add migration

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,48 @@
-import express from "express";
-import { isValidCPF } from "./cpf/cpfValidator";
-import pgp from "pg-promise";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { isValidCPF } from '@src/cpf/cpfValidator';
+
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-
-// const products = [
-// 	{ idProduct: 1, description: "A", price: 1000 },
-// 	{ idProduct: 2, description: "B", price: 5000 },
-// 	{ idProduct: 3, description: "C", price: 30 }
-// ];
-
-// const coupons = [
-// 	{ code: "VALE20", percentage: 20 }
-// ];
-
-app.post("/checkout", async function (req, res) {
-	const isValid = isValidCPF(req.body.cpf);
-	if (!isValid) {
-		return res.status(422).json({
-			message: "Invalid cpf"
-		});
-	}
-	let total = 0;
-	for (const item of req.body.items) {
-		// const product = products.find((product) => product.idProduct === item.idProduct);
-		const [product] = await connection.query("select * from cccat9.product where id_product = $1", [item.idProduct]);
-		if (product) {
-			total += parseFloat(product.price) * item.quantity;
-		} else {
-			return res.status(422).json({
-				message: "Product not found"
-			});
-		}
-	}
-	if (req.body.coupon) {
-		// const coupon = coupons.find(coupon => coupon.code === req.body.coupon);
-		const [coupon] = await connection.query("select * from cccat9.coupon where code = $1", [req.body.coupon]);
-		if (coupon) {
-			total -= (total * coupon.percentage)/100;
-		}
-	}
-	res.json({
-		total
-	});
+app.post('/checkout', async function (req, res) {
+  const isValid = isValidCPF(req.body.cpf);
+  if (!isValid) {
+    return res.status(422).json({
+      message: 'Invalid cpf',
+    });
+  }
+  let total = 0;
+  for (const item of req.body.items) {
+    const product = await prisma.product.findUnique({
+      where: {
+        id_product: item.idProduct,
+      },
+    });
+    if (product) {
+      total += product.price * item.quantity;
+    } else {
+      await prisma.$disconnect();
+      return res.status(422).json({
+        message: 'Product not found',
+      });
+    }
+  }
+  if (req.body.coupon) {
+    const coupon = await prisma.coupon.findUnique({
+      where: {
+        code: req.body.coupon,
+      },
+    });
+    if (coupon) {
+      total -= (total * coupon.percentage) / 100;
+    }
+  }
+  await prisma.$disconnect();
+  res.json({
+    total,
+  });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT, () => console.log(`Server listening on ${process.env.PORT}`));
